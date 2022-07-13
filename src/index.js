9bd064b8566819428dd45bafb9aabc06693b7e1f
Donatella7 <campaniellodonatella@gmail.com>
2022-07-13 12:57:24 +0200

    Complete tic tac toe

--- src/index.js
+++ src/index.js
@@ -41,13 +41,18 @@ class Board extends React.Component {
   handleClick(i) {
 
     const squares = this.state.squares.slice();
+
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    
     squares[i] = this.state.xIsNext ? 'x' : 'o';
 
     this.setState({ 
 
       squares: squares,
       xIsNext: !this.state.xIsNext,
-      
+
     });
   }
 
@@ -61,7 +66,18 @@ class Board extends React.Component {
   /* Qui sono definiti i valori pasati al metodo render della classe Square */
   render() {
 
-    const status = 'Next player: ' + (this.state.xIsNext ? 'x' : 'o');
+    const winner = calculateWinner(this.state.squares);
+
+    let status;
+    if(winner) {
+
+      status = 'Winner: ' + winner;
+
+    } else {
+
+      status = 'Next player: ' + (this.state.xIsNext ? 'x' : 'o');
+
+    }
 
     return (
       <div>
@@ -107,6 +123,26 @@ class Game extends React.Component {
   }
 }
 
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
